0ebdc0f374ff3a7b24b194c4745a864d75e4ef45
junyub301 <junyub0301@gmail.com>
2021-07-19 23:43:49 +0900

    AppLoading

--- App.js
+++ App.js
@@ -1,8 +1,28 @@
+import AppLoading from "expo-app-loading";
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
 import { StyleSheet, Text, View } from "react-native";
+import * as Font from "expo-font";
 
 export default function App() {
+    const [loading, setLoading] = useState(true);
+    const onFinish = () => setLoading(false);
+    const preload = () => {
+        const fontToLoad = [Ionicons.font];
+        const fontPromises = fontToLoad.map((font) => Font.loadAsync(font));
+        console.log(fontPromises);
+        return Promise.all(fontPromises);
+    };
+    if (loading) {
+        return (
+            <AppLoading
+                startAsync={preload}
+                onError={console.warn}
+                onFinish={onFinish}
+            />
+        );
+    }
     return (
         <View style={styles.container}>
             <Text>Hello!</Text>
