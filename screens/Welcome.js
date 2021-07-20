fe715ce2db1418c464701fb0d7f6fd7c65796932
junyub301 <junyub0301@gmail.com>
2021-07-20 22:13:49 +0900

    First Navigation

---
+++ screens/Welcome.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { View, Text } from "react-native";
+export default function Welcome() {
+    return (
+        <View>
+            <Text>Welcome</Text>
+        </View>
+    );
+}
