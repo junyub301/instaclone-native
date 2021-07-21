dd5fdf27699a8852b15b7e488a9b7e792f5231c7
junyub301 <junyub0301@gmail.com>
2021-07-21 22:16:39 +0900

    Navigator Props

--- navigators/LoggedOutNav.js
+++ navigators/LoggedOutNav.js
@@ -8,6 +8,12 @@ const Stack = createStackNavigator();
 
 export default function LoggedOutNav() {
     return (
+        /* 
+            Statck.Navigator props 
+            - 네비게이터 props : 전역 설정이 가능 ex) mode, headerMode...
+            - screenOptions : 모든 스크린이 똑같이 보이도록 설정 ex) title,headerTintColor ...
+
+        */
         <Stack.Navigator
             screenOptions={{
                 headerBackTitleVisible: false,
