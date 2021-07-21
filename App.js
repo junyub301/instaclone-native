36272a07add42fef0d3f72c98461876767a27676
junyub301 <junyub0301@gmail.com>
2021-07-21 22:45:15 +0900

    Dark Mode

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-import AppLoading from "expo-app-loading";
-import React, { useState } from "react";
 import { Ionicons } from "@expo/vector-icons";
-import * as Font from "expo-font";
-import { Asset } from "expo-asset";
-import LoggedOutNav from "./navigators/LoggedOutNav";
 import { NavigationContainer } from "@react-navigation/native";
+import AppLoading from "expo-app-loading";
+import { Asset } from "expo-asset";
+import * as Font from "expo-font";
+import React, { useState } from "react";
+import LoggedOutNav from "./navigators/LoggedOutNav";
 
 export default function App() {
     const [loading, setLoading] = useState(true);
