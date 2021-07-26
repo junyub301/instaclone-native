f24282eca7fca51ac88909b81b96a5f85e1e8fd7
junyub301 <junyub0301@gmail.com>
2021-07-26 22:44:36 +0900

    AsyncStorage part 2

--- App.js
+++ App.js
@@ -6,25 +6,30 @@ import * as Font from "expo-font";
 import React, { useState } from "react";
 import LoggedOutNav from "./navigators/LoggedOutNav";
 import { ApolloProvider, useReactiveVar } from "@apollo/client";
-import client, { isLoggedInVar } from "./apollo";
+import client, { isLoggedInVar, tokenVar } from "./apollo";
 import LoggedInNav from "./navigators/LoggedInNav";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function App() {
     const [loading, setLoading] = useState(true);
     const onFinish = () => setLoading(false);
     const isLoggedIn = useReactiveVar(isLoggedInVar);
-
-    const preload = () => {
+    const preloadAssets = () => {
         const fontToLoad = [Ionicons.font];
         const fontPromises = fontToLoad.map((font) => Font.loadAsync(font));
-        const imagesToLoad = [
-            require("./assets/logo.png"),
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/840px-Instagram_logo.svg.png",
-        ];
+        const imagesToLoad = [require("./assets/logo.png")];
         const imagePromises = imagesToLoad.map((image) =>
             Asset.loadAsync(image)
         );
         return Promise.all([...fontPromises, ...imagePromises]);
+    };
+    const preload = async () => {
+        const token = await AsyncStorage.getItem("token");
+        if (token) {
+            isLoggedInVar(true);
+            tokenVar(token);
+        }
+        return preloadAssets();
     };
     if (loading) {
         return (
