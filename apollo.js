0ce29f4b48709f9eb811eae841cf99ba6800254d
junyub301 <junyub0301@gmail.com>
2021-08-03 21:04:06 +0900

    Infinite Scrolling part 2

--- apollo.js
+++ apollo.js
@@ -6,6 +6,7 @@ import {
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { offsetLimitPagination } from "@apollo/client/utilities";
 
 const TOKEN = "token";
 
@@ -39,6 +40,25 @@ const authLink = setContext((_, { headers }) => {
 
 const client = new ApolloClient({
     link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
+    cache: new InMemoryCache({
+        // typePolicies : apollo 에게 type을 설정할 수 있도록 해주는 것.
+        typePolicies: {
+            Query: {
+                fields: {
+                    seeFeed: offsetLimitPagination(),
+                    /* 위와 같은 기능
+                        seeFeed: {
+                            // apollo가 query들을 argument에 따라 구별시키는걸 막아준다.
+                            keyArgs: false,
+                            // 기존의 데이터와 새로 가져오 데이터를 합쳐준다.
+                            merge(exisiting = [], incoming = []) {
+                                return [...exisiting, ...incoming];
+                            },
+                        }, 
+                    */
+                },
+            },
+        },
+    }),
 });
 export default client;
--- screens/Feed.js
+++ screens/Feed.js
@@ -6,8 +6,8 @@ import ScreenLayout from "../components/ScreenLayout";
 import Photo from "../components/Photo";
 
 const FEED_QUERY = gql`
-    query seeFeed {
-        seeFeed {
+    query seeFeed($offset: Int!) {
+        seeFeed(offset: $offset) {
             ...PhotoFragment
             user {
                 username
@@ -26,7 +26,10 @@ const FEED_QUERY = gql`
 `;
 
 export default function Feed({ navigation }) {
-    const { data, loading, refetch } = useQuery(FEED_QUERY);
+    // fetchMore : 기존 결과를 유지한 채 새로운 결과를 추가하는 방식
+    const { data, loading, refetch, fetchMore } = useQuery(FEED_QUERY, {
+        variables: { offset: 0 },
+    });
     const renderPohoto = ({ item: photo }) => {
         return <Photo {...photo} />;
     };
@@ -38,7 +41,19 @@ export default function Feed({ navigation }) {
     const [refreshing, setRefreshing] = useState(false);
     return (
         <ScreenLayout loading={loading}>
+            {/* 
+        onEndReachedThreshold : 리스트 끝이 어딘지를 설정해주는 함수 0이상의 숫자를 값으로 가진다.
+        onEndReached : 리스트 끝에 도달했을 때 호출되는 함수 
+        */}
             <FlatList
+                onEndReachedThreshold={0.05}
+                onEndReached={() =>
+                    fetchMore({
+                        variables: {
+                            offset: data?.seeFeed.length,
+                        },
+                    })
+                }
                 refreshing={refreshing}
                 onRefresh={refresh}
                 style={{ width: "100%" }}
