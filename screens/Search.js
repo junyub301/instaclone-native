93b71c7958627e4adc2cf7bb782650f672ffc47a
junyub301 <junyub0301@gmail.com>
2021-08-16 20:43:34 +0900

    Search part 4

--- screens/Search.js
+++ screens/Search.js
@@ -1,9 +1,16 @@
+import { gql, useLazyQuery } from "@apollo/client";
 import React, { useEffect } from "react";
-import { View, Text, TextInput, ActivityIndicator } from "react-native";
+import { useForm } from "react-hook-form";
+import {
+    ActivityIndicator,
+    FlatList,
+    TouchableOpacity,
+    useWindowDimensions,
+    View,
+    Image,
+} from "react-native";
 import styled from "styled-components/native";
 import DismissKeyboard from "../components/DismissKeyboard";
-import { useForm } from "react-hook-form";
-import { gql, useLazyQuery } from "@apollo/client";
 
 const SEARCH_PHOTOS = gql`
     query searchPhotos($keyword: String!) {
@@ -25,9 +32,17 @@ const MessageText = styled.Text`
     font-weight: 600;
 `;
 
-const Inpupt = styled.TextInput``;
+const Inpupt = styled.TextInput`
+    background-color: rgba(255, 255, 255, 1);
+    color: black;
+    width: ${(props) => props.width / 1.5}px;
+    padding: 5px 10px;
+    border-radius: 7px;
+`;
 
 export default function Search({ navigation }) {
+    const numColumns = 4;
+    const { width } = useWindowDimensions();
     const { setValue, register, watch, handleSubmit } = useForm();
     // useQuery를 component가 mount될때 즉시 실행 하지만 useLazyQuery는 바로 실행되지 않는다.
     const [startQueryFn, { loading, data, called }] = useLazyQuery(
@@ -40,11 +55,11 @@ export default function Search({ navigation }) {
             },
         });
     };
-    console.log(data);
     const SearchBox = () => (
-        <TextInput
+        <Inpupt
+            width={width}
             style={{ backgroundColor: "white" }}
-            placeholderTextColor='black'
+            placeholderTextColor='rgba(0,0,0,0.8)'
             placeholder='Search photos'
             autoCapitalize='none'
             returnKeyLabel='Search'
@@ -60,6 +75,15 @@ export default function Search({ navigation }) {
         });
         register("keyword", { required: true, minLength: 3 });
     }, []);
+
+    const renderitem = ({ item: photo }) => (
+        <TouchableOpacity>
+            <Image
+                source={{ uri: photo.file }}
+                style={{ width: width / numColumns, height: 100 }}
+            />
+        </TouchableOpacity>
+    );
     return (
         <DismissKeyboard>
             <View style={{ flex: 1, backgroundColor: "black" }}>
@@ -74,11 +98,19 @@ export default function Search({ navigation }) {
                         <MessageText>Search by keyword</MessageText>
                     </MessageContainer>
                 ) : null}
-                {data?.searchPhotos !== undefined &&
-                data?.searchPhotos?.length === 0 ? (
-                    <MessageContainer>
-                        <MessageText>Could not find anything.</MessageText>
-                    </MessageContainer>
+                {data?.searchPhotos !== undefined ? (
+                    data?.searchPhotos?.length === 0 ? (
+                        <MessageContainer>
+                            <MessageText>Could not find anything.</MessageText>
+                        </MessageContainer>
+                    ) : (
+                        <FlatList
+                            numColumns={numColumns}
+                            data={data?.searchPhotos}
+                            keyExtractor={(photo) => "" + photo.id}
+                            renderItem={renderitem}
+                        />
+                    )
                 ) : null}
             </View>
         </DismissKeyboard>
